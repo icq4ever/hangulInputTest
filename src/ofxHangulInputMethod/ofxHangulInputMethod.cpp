30f227c2e264f58db739ebe3edaab9f8f665ccfe
Yi donghoon <icq4ever@gmail.com>
2017-05-07 17:09:13 +0900

    ignore

---
+++ src/ofxHangulInputMethod/ofxHangulInputMethod.cpp
@@ -0,0 +1,13 @@
+//
+//  ofxHangulInputMethod.cpp
+//  hangulInputTest
+//
+//  Created by icq4ever on 07/05/2017.
+//
+//
+
+#include "ofxHangulInputMethod.h"
+
+ofxHangulInputMethod::ofxHangulInputMethod(){
+
+}
